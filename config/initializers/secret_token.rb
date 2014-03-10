d01588f9ef6b312e15c4ae35272460f2824932d8
mizdler <elahi2m@yahoo.com>
2014-03-10 11:07:57 +0330

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Baazino::Application.config.secret_key_base = '8eee8a829b16f7cbcc581a813592eb4385dcd382f7225522db22faa70fe10717150aa558feb00c9618cc119882b6abe4aeb3dfc979707b0ecd45f5570826919c'
