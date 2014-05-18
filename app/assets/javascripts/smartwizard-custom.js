946b5469cb6915fd122953dc971c70a2f25dbf08
mizdler <elahi2m@yahoo.com>
2014-05-18 09:59:11 +0430

    Manovr version.

--- app/assets/javascripts/smartwizard-custom.js
+++ app/assets/javascripts/smartwizard-custom.js
@@ -1,12 +1,17 @@
-    $(document).ready(function(){
-    	// Smart Wizard 	
-  		$('#wizard').smartWizard();
-      	$('#wizardVertical').smartWizard({transitionEffect:'slide'});
-     
-		
+$(document).ready(function() {
+	// Smart Wizard
+	$('#wizard').smartWizard({
+		labelNext : 'بعدی', // label for Next button
+		labelPrevious : 'قبلی', // label for Previous button
+		labelFinish : 'پایان',  // label for Finish button
+	});
+	$('#wizardVertical').smartWizard({
+		transitionEffect : 'slide'
+	});
 
-      function onFinishCallback(){
-        $('#wizard').smartWizard('showMessage','Finish Clicked');
-        //alert('Finish Clicked');
-      }     
-		});
+	function onFinishCallback() {
+		$('#wizard').smartWizard('showMessage', 'Finish Clicked');
+		//alert('Finish Clicked');
+	}
+
+});
